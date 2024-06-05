c68178f1d7f8bb677aa80eab0afc50030689059c
hnoj <mrocznaklawiatura@gmail.com>
2024-06-05 20:24:21 +0200

    za malo destrukcji :D/

--- DsLauncher.ApiClient/DsLauncherClient.cs
+++ DsLauncher.ApiClient/DsLauncherClient.cs
@@ -2015,14 +2015,14 @@ namespace DsLauncher.ApiClient
         }
 
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual System.Threading.Tasks.Task<FileResponse> Ndib_GetUpdate2Async(System.Guid srcGuid, Platform platform)
+        public virtual System.Threading.Tasks.Task<FileResponse> Ndib_GetUpdateToLatestAsync(System.Guid srcGuid, Platform platform)
         {
-            return Ndib_GetUpdate2Async(srcGuid, platform, System.Threading.CancellationToken.None);
+            return Ndib_GetUpdateToLatestAsync(srcGuid, platform, System.Threading.CancellationToken.None);
         }
 
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual async System.Threading.Tasks.Task<FileResponse> Ndib_GetUpdate2Async(System.Guid srcGuid, Platform platform, System.Threading.CancellationToken cancellationToken)
+        public virtual async System.Threading.Tasks.Task<FileResponse> Ndib_GetUpdateToLatestAsync(System.Guid srcGuid, Platform platform, System.Threading.CancellationToken cancellationToken)
         {
             if (srcGuid == null)
                 throw new System.ArgumentNullException("srcGuid");
@@ -2124,11 +2124,99 @@ namespace DsLauncher.ApiClient
 
                     var urlBuilder_ = new System.Text.StringBuilder();
                 
-                    // Operation Path: "Ndib/download/{productGuid}/{platform}"
-                    urlBuilder_.Append("Ndib/download/");
+                    // Operation Path: "Ndib/download/whole/{productGuid}/{platform}"
+                    urlBuilder_.Append("Ndib/download/whole/");
                     urlBuilder_.Append(System.Uri.EscapeDataString(ConvertToString(productGuid, System.Globalization.CultureInfo.InvariantCulture)));
                     urlBuilder_.Append('/');
                     urlBuilder_.Append(System.Uri.EscapeDataString(ConvertToString(platform, System.Globalization.CultureInfo.InvariantCulture)));
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>>();
+                        foreach (var item_ in response_.Headers)
+                            headers_[item_.Key] = item_.Value;
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200 || status_ == 206)
+                        {
+                            var responseStream_ = response_.Content == null ? System.IO.Stream.Null : await response_.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                            var fileResponse_ = new FileResponse(status_, headers_, responseStream_, null, response_);
+                            disposeClient_ = false; disposeResponse_ = false; // response and client are disposed by FileResponse
+                            return fileResponse_;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual System.Threading.Tasks.Task<FileResponse> Ndib_GetWholeVersionAsync(System.Guid productGuid, Platform platform, System.Guid packageGuid)
+        {
+            return Ndib_GetWholeVersionAsync(productGuid, platform, packageGuid, System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual async System.Threading.Tasks.Task<FileResponse> Ndib_GetWholeVersionAsync(System.Guid productGuid, Platform platform, System.Guid packageGuid, System.Threading.CancellationToken cancellationToken)
+        {
+            if (productGuid == null)
+                throw new System.ArgumentNullException("productGuid");
+
+            if (platform == null)
+                throw new System.ArgumentNullException("platform");
+
+            if (packageGuid == null)
+                throw new System.ArgumentNullException("packageGuid");
+
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/octet-stream"));
+
+                    var urlBuilder_ = new System.Text.StringBuilder();
+                
+                    // Operation Path: "Ndib/download/whole/{productGuid}/{platform}/{packageGuid}"
+                    urlBuilder_.Append("Ndib/download/whole/");
+                    urlBuilder_.Append(System.Uri.EscapeDataString(ConvertToString(productGuid, System.Globalization.CultureInfo.InvariantCulture)));
+                    urlBuilder_.Append('/');
+                    urlBuilder_.Append(System.Uri.EscapeDataString(ConvertToString(platform, System.Globalization.CultureInfo.InvariantCulture)));
+                    urlBuilder_.Append('/');
+                    urlBuilder_.Append(System.Uri.EscapeDataString(ConvertToString(packageGuid, System.Globalization.CultureInfo.InvariantCulture)));
 
                     PrepareRequest(client_, request_, urlBuilder_);
 
